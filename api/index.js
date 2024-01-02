72185f29e7cecb469539880668935151b79bc906
SantiPaper <santiagovitelli01@gmail.com>
2024-01-02 20:32:48 -0300

    config eslint

--- api/index.js
+++ api/index.js
@@ -1,17 +1,22 @@
-import express from 'express'
+import express from "express";
+import axios from "axios";
 
 const app = express();
 
 const puerto = 3001;
 
-app.get("/", (req,res) => {
-    
-    res.json({
-        message:'Hello'
-    })
-})
+const baseUrl = "https://api.mercadolibre.com";
 
-app.listen(puerto,() => {
-    console.log(`Se esta escuchando en el puerto ${puerto}`);
+app.get("/", (req, res) => {
+  const { q } = req.query;
+
+  axios.get(`${baseUrl}/sites/MLA/search?q=${q}`).then(({ data }) => {
+    res.json({
+      data,
+    });
+  });
 });
 
+app.listen(puerto, () => {
+  console.log(`Se esta escuchando en el puerto ${puerto}`);
+});
