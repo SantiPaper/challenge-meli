70b540cb5dbf87ce8711e8a6a85a756bc975594b
SantiPaper <santiagovitelli01@gmail.com>
2024-01-03 16:25:47 -0300

    get products ruta

--- api/index.js
+++ api/index.js
@@ -7,14 +7,44 @@ const puerto = 3001;
 
 const baseUrl = "https://api.mercadolibre.com";
 
-app.get("/", (req, res) => {
+const utilsAuthor = {
+  name: "Santiago",
+  lastname: "Vitelli",
+};
+
+app.get("/", async (req, res) => {
   const { q } = req.query;
 
-  axios.get(`${baseUrl}/sites/MLA/search?q=${q}`).then(({ data }) => {
+  try {
+    const { data } = await axios.get(`${baseUrl}/sites/MLA/search?q=${q}`);
+    const categoryId = data.results[0].category_id;
+
+    const { data: categories } = await axios.get(
+      `${baseUrl}/categories/${categoryId}`
+    );
+
     res.json({
-      data,
+      author: utilsAuthor,
+      categories: categories.path_from_root.map((category) => category.name),
+      items: [
+        data.results.slice(0, 4).map((product) => ({
+          id: product.id,
+          title: product.title,
+          price: {
+            currency: product.currency_id,
+            amount: product.price,
+
+            decimals: product.price.toString().split("."),
+          },
+          picture: product.thumbnail,
+          condition: product.condition,
+          free_shipping: product.shipping.free_shipping,
+        })),
+      ],
     });
-  });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(puerto, () => {
