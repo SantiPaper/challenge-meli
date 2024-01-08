1c13f2a07a3380eb8b7941635ec94d6b1062c6df
SantiPaper <santiagovitelli01@gmail.com>
2024-01-08 20:02:00 -0300

    add routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./components/Home";
+import { Results } from "./components/Results";
+import { Detail } from "./components/Detail";
+import { Layout } from "./components/Layout";
 function App() {
-  return <h1>Hola</h1>;
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/items" element={<Results />} />
+        <Route path="/items/:id" element={<Detail />} />
+      </Routes>
+    </Layout>
+  );
 }
 
 export default App;
