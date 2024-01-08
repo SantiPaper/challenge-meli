a4936a90cbe6065f4c30444a79d4709912bc9e6d
SantiPaper <santiagovitelli01@gmail.com>
2024-01-08 20:04:53 -0300

    add navbar component

---
+++ client/src/components/Navbar/index.tsx
@@ -0,0 +1,19 @@
+import { Container } from "../Container";
+import { SearchInput } from "../SearchInput";
+import { StyledNavbar } from "./style";
+import { Link } from "react-router-dom";
+
+export const Navbar = () => {
+  return (
+    <StyledNavbar>
+      <Container>
+        <div className="navbar__content">
+          <Link to="/">
+            <img width={70} src="/images/logo_meli.png" alt="Logo" />
+          </Link>
+          <SearchInput />
+        </div>
+      </Container>
+    </StyledNavbar>
+  );
+};
---
+++ client/src/components/Navbar/style.ts
@@ -0,0 +1,13 @@
+import {styled} from "styled-components"
+
+export const StyledNavbar = styled.header`
+background-color: var(--yellow);
+padding: 1rem 0;
+
+.navbar__content{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+}
+`
