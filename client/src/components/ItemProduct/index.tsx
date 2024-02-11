5ba65c85fed5017a53a7998fded6f0ea351daaff
Santiago Vitelli <santiagovitelli01@gmail.com>
2024-02-11 14:32:57 -0300

    style itemproducts

--- client/src/components/ItemProduct/index.tsx
+++ client/src/components/ItemProduct/index.tsx
@@ -1,12 +1,31 @@
 import { StyledItemProduct } from "./style";
+import imgShipping from "../../../public/images/shipping.png";
 
 type Props = {
   title: string;
+  price: number;
+  img: string;
+  shipping: boolean;
+  id: string;
 };
-export const ItemProduct = ({ title }: Props) => {
+export const ItemProduct = ({ title, price, img, shipping, id }: Props) => {
   return (
-    <StyledItemProduct>
-      <h3>{title}</h3>
+    <StyledItemProduct id={id}>
+      <img
+        className="product__img"
+        src={img}
+        alt={title}
+        width={180}
+        height={180}
+      />
+
+      <div className="product__info">
+        <div className="product__info__price-container">
+          <p className="product__info__price-container__price">$ {price}</p>
+          {shipping && <img src={imgShipping} alt="" width={15} height={15} />}
+        </div>
+        <h3 className="product__info__title">{title}</h3>
+      </div>
     </StyledItemProduct>
   );
 };
--- client/src/components/ItemProduct/style.ts
+++ client/src/components/ItemProduct/style.ts
@@ -1,5 +1,31 @@
 import styled from "styled-components";
 
 export const StyledItemProduct = styled.article`
-  background-color: aliceblue;
+  display: flex;
+  .product {
+    &__img {
+      padding: var(--spacing-m);
+      border-radius: var(--radius);
+    }
+
+    &__info {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+
+      &__price-container {
+        display: flex;
+        margin-bottom: var(--spacing-l);
+        gap: 0.5rem;
+        align-items: center;
+
+        &__price {
+          font-size: var(--font-size-500);
+        }
+      }
+      &__title {
+        font-size: var(--font-size-400);
+      }
+    }
+  }
 `;
