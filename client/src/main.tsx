9cd5ef30875866b0af56612ed3767547b04500df
Santiago Vitelli <santiagovitelli01@gmail.com>
2024-02-11 06:02:24 -0300

    add provider in main

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "./context/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
