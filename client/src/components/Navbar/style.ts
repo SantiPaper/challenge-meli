1c3b73717cc3961f9cb8eb43d87080bb1d01a573
Santiago Vitelli <santiagovitelli01@gmail.com>
2024-02-11 06:01:25 -0300

    change searchinput

--- client/src/components/Navbar/style.ts
+++ client/src/components/Navbar/style.ts
@@ -2,9 +2,13 @@ import { styled } from "styled-components";
 
 export const StyledNavbar = styled.header`
   background-color: var(--yellow);
-  padding: 1rem 0;
+  display: flex;
+  justify-content: center;
+  width: 100%;
 
   .navbar__content {
+    padding: 1rem 0;
+    width: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
--- client/src/components/SearchInput/index.tsx
+++ client/src/components/SearchInput/index.tsx
@@ -1,19 +1,36 @@
 import { StyledSearchInput } from "./style";
+import { useProductsContext } from "../../hooks/useProductsContext";
+import { useNavigate } from "react-router-dom";
 
 export const SearchInput = () => {
+  const { state, getSearchString } = useProductsContext();
+  const navigate = useNavigate();
+
+  const handleChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    getSearchString(ev.target.value);
+  };
+
+  const handleSubmit = (ev: React.FormEvent<HTMLFormElement>) => {
+    ev.preventDefault();
+    if (state.searchInput) {
+      navigate(`/items?search=${state.searchInput}`);
+    }
+  };
+
   return (
-    <StyledSearchInput>
+    <StyledSearchInput onSubmit={handleSubmit}>
       <label htmlFor="search" className="sr-only">
-        Buscar producto
+        Buscar productos
       </label>
       <input
+        onChange={handleChange}
         className="search__input"
         placeholder="Nunca dejes de buscar"
-        type="text"
+        type="search"
         id="search"
         name="search"
       />
-      <button className="search__button" aria-label="Buscar">
+      <button className="search__button" aria-label="Buscar" type="submit">
         <img width={20} src="/images/search.png" alt="Buscar" />
       </button>
     </StyledSearchInput>
