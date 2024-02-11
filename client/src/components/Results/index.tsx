0554041c86d5c2627a82eae0584683b85aed6537
Santiago Vitelli <santiagovitelli01@gmail.com>
2024-02-11 06:01:45 -0300

    create results component

--- client/src/components/Results/index.tsx
+++ client/src/components/Results/index.tsx
@@ -1,11 +1,33 @@
 import { Background } from "../Background";
-import { Container } from "../Container";
+import { StyledResults } from "./style";
+import { useProductsContext } from "../../hooks/useProductsContext";
+import { useEffect } from "react";
 
 export const Results = () => {
+  const { state, getProductsBySearch } = useProductsContext();
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/api/items`)
+      .then((data) => data.json())
+      .then((data) => {
+        getProductsBySearch(data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  console.log(state);
+
   return (
-    <Background>
-      <p>Camisas ,, Pantalones ,, Indumentaria</p>
-      <h1>Hola gente</h1>
-    </Background>
+    <StyledResults>
+      {state.searchResults.categories ? (
+        <p className="results__categories">{state.searchResults.categories}</p>
+      ) : (
+        <p>Tiraste cualquiera</p>
+      )}
+
+      <Background>
+        <h1>Hola gente</h1>
+      </Background>
+    </StyledResults>
   );
 };
---
+++ client/src/components/Results/style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledResults = styled.section`
+  .results {
+    &__categories {
+      margin: var(--spacing-m) 0;
+    }
+  }
+`;
