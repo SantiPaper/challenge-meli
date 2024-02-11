365e165fc47bb5dfb70a5811b36d097bbd1f0ae9
Santiago Vitelli <santiagovitelli01@gmail.com>
2024-02-11 15:10:19 -0300

    change text

--- client/src/components/Home/index.tsx
+++ client/src/components/Home/index.tsx
@@ -3,10 +3,7 @@ import { StyledHome } from "./style";
 export const Home = () => {
   return (
     <StyledHome>
-      <h1 className="hero">
-        Aun no realizasteaaaaaaaaa ninguna busqueda, te alentamos a que hagas
-        una!
-      </h1>
+      <h1 className="hero">Estamos en home</h1>
     </StyledHome>
   );
 };
