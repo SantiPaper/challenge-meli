149ac58dfd4b998136f03b49f6c180dee870516d
Santiago Vitelli <santipapor@gmail.com>
2024-02-12 10:16:38 -0300

    create loading component

---
+++ client/src/components/Loading/index.tsx
@@ -0,0 +1,9 @@
+import { StyledLoading } from "./style";
+
+export const Loading = () => {
+  return (
+    <StyledLoading>
+      <h2 className="text">Cargando...</h2>
+    </StyledLoading>
+  );
+};
---
+++ client/src/components/Loading/style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledLoading = styled.section`
+  display: flex;
+  justify-content: center;
+  margin-top: 8rem;
+
+  .text {
+    font-size: var(--font-size-500);
+  }
+`;
