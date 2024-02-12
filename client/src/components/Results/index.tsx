eddf3d50b674b4ce7e9f6267b2f51a788e7251af
Santiago Vitelli <santipapor@gmail.com>
2024-02-12 10:20:20 -0300

    update results

--- client/src/components/Results/index.tsx
+++ client/src/components/Results/index.tsx
@@ -1,39 +1,58 @@
-import { StyledResults } from "./style";
+import React from "react";
+import { useSearchParams } from "react-router-dom";
 import { useProductsContext } from "../../hooks/useProductsContext";
 import { useFetch } from "../../hooks/useFetch";
-
-import { useSearchParams } from "react-router-dom";
 import { Background } from "../Background";
 import { ItemProduct } from "../ItemProduct";
+import { Breadcrumbs } from "../Breadcrumbs";
+import { StyledResults } from "./style";
+import { Loading } from "../Loading";
 
 export const Results = () => {
   const { state } = useProductsContext();
   const [params] = useSearchParams();
+
   const { loading, error } = useFetch({
     status: "?q=",
     payload: params.get("search"),
   });
 
+  console.log(JSON.stringify(error));
+
   return (
     <StyledResults>
-      {error && <p>{error}</p>}
       {loading ? (
-        <p>Loading component</p>
+        <Loading />
       ) : (
         <>
-          <p className="results__categories">
-            {state.searchResults.categories}
-          </p>
+          {state.searchResults.categories && (
+            <Breadcrumbs categories={state.searchResults.categories} />
+          )}
           <Background>
-            {state.searchResults.items.map((prod) => (
-              <ItemProduct
-                key={prod.id}
-                title={prod.title}
-                price={prod.price.amount}
-                shipping={prod.free_shipping}
-                img={prod.picture}
-              />
-            ))}
+            {state.searchResults.items ? (
+              state.searchResults.items.map((prod) => {
+                const number = new Intl.NumberFormat("es-AR", {
+                  style: "currency",
+                  currency: "ARS",
+                  minimumFractionDigits: 0,
+                  maximumFractionDigits: 0,
+                }).format(prod.price.amount);
+
+                return (
+                  <ItemProduct
+                    key={prod.id}
+                    id={prod.id}
+                    title={prod.title}
+                    price={number}
+                    shipping={prod.free_shipping}
+                    img={prod.picture}
+                    condition={prod.condition}
+                  />
+                );
+              })
+            ) : (
+              <h2>{error}</h2>
+            )}
           </Background>
         </>
       )}
--- client/src/components/Results/style.ts
+++ client/src/components/Results/style.ts
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const StyledResults = styled.section`
   .results {
     &__categories {
-      margin: var(--spacing-m) 0;
     }
   }
 `;
