b4a8d58ebb0d0a1f2c8ed8b7e773bd83f35b6077
SantiPaper <gonsanto07@yahoo.com>
2024-02-14 22:00:04 -0300

    update useFetch custom hook

--- client/src/hooks/useFetch.ts
+++ client/src/hooks/useFetch.ts
@@ -7,22 +7,34 @@ type Props = {
 };
 
 export const useFetch = ({ status, payload }: Props) => {
-  const { getProductsBySearch } = useProductsContext();
+  const { getProductsBySearch, getProductById } = useProductsContext();
 
   const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     setLoading(true);
+    console.log(`http://localhost:3001/api/items${status}${payload}`);
+
     fetch(`http://localhost:3001/api/items${status}${payload}`)
-      .then((data) => data.json())
-      .then((data) => {
-        getProductsBySearch(data);
+      .then((response) => {
+        if (response.status === 404) {
+          throw new Error(`Producto no encontrado`);
+        }
+        return response.json();
       })
-      .catch((error) => setError(error))
-      .finally(() => {
-        setLoading(false);
-      });
+      .then((data) => {
+        if (status === "/") {
+          getProductById(data);
+          setError(null);
+        } else {
+          getProductsBySearch(data);
+          setError(null);
+        }
+      })
+      .catch((e) => setError(e.message))
+      .finally(() => setLoading(false));
   }, []);
+
   return { loading, error };
 };
