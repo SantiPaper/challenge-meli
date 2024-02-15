1e3b1babe1cc8e07a1e562d99f8e0c0a8e13019c
SantiPaper <gonsanto07@yahoo.com>
2024-02-14 21:59:51 -0300

    update context

--- client/src/types/context.ts
+++ client/src/types/context.ts
@@ -1,16 +1,23 @@
-import { SearchResults } from "./items";
+import { ProductDetail, SearchResults } from "./items";
 
 export type Context = {
   state: State;
   getProductsBySearch: (searchResults: SearchResults) => void;
+  getProductById: (product: ProductDetail) => void;
 };
 
 export type State = {
   searchResults: SearchResults;
+  product: ProductDetail;
 };
 export type Reducer = (state: State, action: Action) => State;
 
-export type Action = {
-  type: "GET_PRODUCTS";
-  payload: SearchResults;
-};
+export type Action =
+  | {
+      type: "GET_PRODUCTS";
+      payload: SearchResults;
+    }
+  | {
+      type: "GET_PRODUCT_BY_ID";
+      payload: ProductDetail;
+    };
