2847665d487a4725d27fee2742da47a35a62533a
SantiPaper <gonsanto07@yahoo.com>
2024-02-18 22:29:34 -0300

    tilde home

--- client/src/components/Home/index.tsx
+++ client/src/components/Home/index.tsx
@@ -4,7 +4,7 @@ export const Home = () => {
   return (
     <StyledHome>
       <h1 className="hero">
-        Aun no realizaste ninguna busqueda, te alentamos a que hagas una!
+        Aun no realizaste ninguna búsqueda, te alentamos a que hagas una!
       </h1>
     </StyledHome>
   );
