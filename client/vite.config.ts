8c7f28fa27b24cd9e0a340bfedf4b91b459a8888
SantiPaper <gonsanto07@yahoo.com>
2024-02-23 11:59:06 -0300

    add config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: "jsdom",
+    alias: {
+      "@/": new URL("./src/", import.meta.url).pathname,
+    },
+    globals: true,
+    include: ["**/?(*.)test.ts?(x)"],
+    setupFiles: ["./tests/unit/setup.ts"],
+  },
+});
