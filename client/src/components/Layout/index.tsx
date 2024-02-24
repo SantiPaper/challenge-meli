79150ae0cc46843a2e66e72ce7ebe94f072b26e2
SantiPaper <gonsanto07@yahoo.com>
2024-02-24 08:32:36 -0300

    change conf props in component

--- client/src/components/Layout/index.tsx
+++ client/src/components/Layout/index.tsx
@@ -1,9 +1,8 @@
 import { PropsWithChildren } from "react";
-import { FC } from "react";
 import { Navbar } from "../Navbar";
 import { Container } from "../Container";
 import { StyledLayout } from "./style";
-export const Layout: FC<PropsWithChildren> = ({ children }) => {
+export const Layout = ({ children }: PropsWithChildren) => {
   return (
     <StyledLayout>
       <Navbar />
