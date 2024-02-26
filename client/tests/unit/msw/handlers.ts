022f3229c507c8fd208dbd55776c5092e5d74727
SantiPaper <gonsanto07@yahoo.com>
2024-02-26 11:26:25 -0300

    add handler in msw

--- client/tests/unit/msw/handlers.ts
+++ client/tests/unit/msw/handlers.ts
@@ -1,10 +1,17 @@
 import { HttpResponse, http } from "msw";
 import { mockCategories, mockProds } from "../app/components/Results/mocks";
+import { mockProduct } from "../app/components/Detail/mocks";
 
 export const handlers = [
   http.get("http://localhost:3001/api/items", () => {
     return HttpResponse.json({
       items: mockProds,
+      categories: mockCategories,
+    });
+  }),
+  http.get("http://localhost:3001/api/items/:id", () => {
+    return HttpResponse.json({
+      item: mockProduct,
       categories: mockCategories,
     });
   }),
